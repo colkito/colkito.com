8acbea0dc738d895107f2a196772f8a0a4be5a8c
Mario Colque <colquemario@gmail.com>
2016-01-14 04:20:04 -0300

    fixed charset

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 5000,
     ip = '0.0.0.0';
 
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
   res.write(JSON.stringify(data, null, 2));
   res.end();
 });
